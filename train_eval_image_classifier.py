6a9749d882a2a8f777233796f6abf91e74747130
wangjian429 <wangjian429@163.com>
2018-07-30 22:41:15 +0800

    modify code for vehicle detecter project

--- train_eval_image_classifier.py
+++ train_eval_image_classifier.py
@@ -9,16 +9,16 @@ import os
 def parse_args(check=True):
     parser = argparse.ArgumentParser()
     # train
-    parser.add_argument('--dataset_name', type=str, default='quiz')
+    parser.add_argument('--dataset_name', type=str, default='vehicle')
     parser.add_argument('--dataset_dir', type=str)
     parser.add_argument('--checkpoint_path', type=str, default='')
     parser.add_argument('--model_name', type=str, default='inception_v4')
     parser.add_argument('--checkpoint_exclude_scopes', type=str, default='InceptionV4/Logits,InceptionV4/AuxLogits/Aux_logits')
     parser.add_argument('--train_dir', type=str)
     parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--clone_on_cpu', type=bool, default=True)
+    parser.add_argument('--clone_on_cpu', type=bool, default=False)
     parser.add_argument('--optimizer', type=str, default='rmsprop')
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=24)
 
     # eval
     parser.add_argument('--dataset_split_name', type=str, default='validation')
@@ -39,19 +39,19 @@ if __name__ == '__main__':
     print('change wording dir to [{0}]'.format(w_d))
     os.chdir(w_d)
 
-    step_per_epoch = 5000 // FLAGS.batch_size
+    step_per_epoch = 41855 // FLAGS.batch_size
 
     if FLAGS.checkpoint_path:
         ckpt = ' --checkpoint_path=' + FLAGS.checkpoint_path
     else:
         ckpt = ''
     for i in range(30):
-        steps = int(step_per_epoch * (i + 1))
+        steps = int(step_per_epoch * (i + 1) * 2)
         # train 1 epoch
         print('################    train    ################')
         p = os.popen(train_cmd.format(**{'dataset_name': FLAGS.dataset_name, 'dataset_dir': FLAGS.dataset_dir,
-                                         'model_name': FLAGS. model_name,
-                                         'checkpoint_exclude_scopes': FLAGS.checkpoint_exclude_scopes, 'train_dir': FLAGS. train_dir,
+                                         'model_name': FLAGS.model_name,
+                                         'checkpoint_exclude_scopes': FLAGS.checkpoint_exclude_scopes, 'train_dir': FLAGS.train_dir,
                                          'learning_rate': FLAGS.learning_rate, 'optimizer': FLAGS.optimizer,
                                          'batch_size': FLAGS.batch_size, 'max_number_of_steps': steps, 'clone_on_cpu': FLAGS.clone_on_cpu}) + ckpt)
         for l in p:
@@ -60,8 +60,8 @@ if __name__ == '__main__':
         # eval
         print('################    eval    ################')
         p = os.popen(eval_cmd.format(**{'dataset_name': FLAGS.dataset_name, 'dataset_dir': FLAGS.dataset_dir,
-                                        'dataset_split_name': FLAGS.dataset_split_name, 'model_name': FLAGS. model_name,
+                                        'dataset_split_name': FLAGS.dataset_split_name, 'model_name': FLAGS.model_name,
                                         'checkpoint_path': FLAGS.train_dir, 'batch_size': FLAGS.batch_size,
-                                        'eval_dir': FLAGS. eval_dir, 'max_num_batches': FLAGS. max_num_batches}))
+                                        'eval_dir': FLAGS.eval_dir, 'max_num_batches': FLAGS.max_num_batches}))
         for l in p:
             print(p.strip())
